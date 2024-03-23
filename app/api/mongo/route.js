745772c08b45c74a62ba7c44d1201009164ccba7
debasisray16 <debasishray3275@gmail.com>
2024-03-23 14:25:13 +0530

    main

--- app/api/mongo/route.js
+++ app/api/mongo/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
 
 
     // Replace the uri string with your connection string.
-    const uri = "mongodb+srv://mongodb:gsgrBo6pI3S8QFU1clusterexpress.6qtmddz.mongodb.net/";
+    const uri = "mongodb+srv://mongodb:gsgrBo6pI3S8QFU1@clusterexpress.6qtmddz.mongodb.net/";
 
     const client = new MongoClient(uri);
 
@@ -16,11 +16,11 @@ export async function GET(request) {
 
         // Query for a movie that has the title 'Back to the Future'
         const query = {};
-        const movie = await movies.findOne(query);
+        const movie = await movies.find(query).toArray();
 
 
         console.log(movie);
-        return NextResponse.json({  movie });
+        return NextResponse.json({movie});
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
